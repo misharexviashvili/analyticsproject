79d6854f81feedb47f4b94b7ce4b7238d24e9748
Misha <misharexviashvili@yahoo.com>
2023-03-22 00:32:19 +0400

    Cards section

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,29 +1,36 @@
-import React from "react";
-import Single from "../assets/single.png";
 import { Card } from "./Card";
 export const Cards = () => {
   return (
-    // <div className="w-full py-[10rem] px-4 bg-white">
-    //   <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
-    //     <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
-    //       <img
-    //         className="w-20 mx-auto mt-[-3rem] bg-white"
-    //         src={Single}
-    //         alt="/"
-    //       />
-    //       <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
-    //       <p className="text-center text-4xl font-bold">$149</p>
-    //       <div className="text-center font-medium">
-    //         <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
-    //         <p className="py-2 border-b mx-8">1 Granted User</p>
-    //         <p className="py-2 border-b mx-8">Send up to 2 GB</p>
-    //       </div>
-    //       <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
-    //         Start Trial
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
-    <Card/>
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <Card
+          src={require("../assets/single.png")}
+          users="Single User"
+          price="$149"
+          storage="500 GB Storage"
+          granted="1 Granted User"
+          upload="Send up to 2 GB"
+        />
+        <Card
+          src={require("../assets/double.png")}
+          users="Parthnership User"
+          price="$249"
+          storage="1 TB Storage"
+          granted="1 Granted User"
+          upload="Send up to 4 GB"
+          style={{ backgroundColor: "#000300", color: "#00df9a" }}
+          middleCardStyle="md:my-0 my-8 bg-gray-100"
+          middleCardImageStyle='bg-transparent'
+        />
+        <Card
+          src={require("../assets/triple.png")}
+          users="Corporate User"
+          price="$549"
+          storage="4 TB Storage"
+          granted="Unlimited Granted Users"
+          upload="Send up to 10 GB"
+        />
+      </div>
+    </div>
   );
 };

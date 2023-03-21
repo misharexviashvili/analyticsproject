e08852123977779255f936e112431da3b3bc2cc7
Misha <misharexviashvili@yahoo.com>
2023-03-21 23:34:57 +0400

    Newsletter section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Analytics } from "./components/Analytics";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
+import { Newsletter } from "./components/Newsletter";
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </div>
   );
 }

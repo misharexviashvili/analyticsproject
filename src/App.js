c71cef2748b7053e342bb0a77e767b5053ae4488
Misha <misharexviashvili@yahoo.com>
2023-03-21 22:39:57 +0400

    analytics section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Analytics } from "./components/Analytics";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 function App() {
   return (
     <div>
       <Navbar />
-      <Hero/>
+      <Hero />
+      <Analytics />
     </div>
   );
 }
